46bd93d8b5800fcf3b72cfbdd0b35b58bc1048bb
Christopher Bell <hexstudiosteam@gmail.com>
2018-03-05 00:33:43 -0600

    Drew a house in "engine/houseworld.html" to test new ID system

--- engine/storyforgejs/storyforge.js
+++ engine/storyforgejs/storyforge.js
@@ -1,4 +1,4 @@
-document.writeln("<script src='https://code.jquery.com/jquery-3.3.1.js' integrity='sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=' crossorigin='anonymous'></script>"); 
+document.writeln("<script src='https://code.jquery.com/jquery-3.3.1.js' integrity='sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=' crossorigin='anonymous'></script>");
 
 document.writeln("<script type='text/javascript' src='storyforgejs/canvas.js'></script>");
 
@@ -14,6 +14,7 @@ document.writeln("<script type='text/javascript' src='storyforgejs/world.js'></s
 */
 var tick_speed = 25;
 var tile_size = 32;
+var mapIndex = 0;
 
 //Canvas
 
