c5f75e3ba34f8970366efb2261fa4a5ce9a6ed77
hexstudios <hexstudiosteam@gmail.com>
2018-03-06 19:22:07 -0800

    Added jquery 3.3.1 to engine source

--- engine/storyforgejs/storyforge.js
+++ engine/storyforgejs/storyforge.js
@@ -1,4 +1,4 @@
-document.writeln("<script src='https://code.jquery.com/jquery-3.3.1.js' integrity='sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=' crossorigin='anonymous'></script>");
+document.writeln("<script src='storyforgejs/jquery-3.3.1.js'></script>");
 document.writeln("<script type='text/javascript' src='storyforgejs/util.js'></script>");
 document.writeln("<script type='text/javascript' src='storyforgejs/canvas.js'></script>");
 document.writeln("<script type='text/javascript' src='storyforgejs/sprite.js'></script>");
